f103ef565eea4b4d12e6f7925399a673875c24bd
jurbraam94 <jur_braam@hotmail.com>
2015-05-28 12:22:55 +0200

    tried to refactor

--- server/app/models/Dood.js
+++ server/app/models/Dood.js
@@ -1,9 +1,10 @@
 ﻿/*jslint node:true */
+
 (function () {
     "use strict";
     /**
-    * Module dependencies.
-    */
+     * Module dependencies.
+     */
     var mongoose = require('mongoose'),
         Schema = mongoose.Schema,
         dood,
@@ -21,97 +22,91 @@
 
 
     algemeneGegevens = new Schema({
-        voornaam: { type: String, required: true },
-        achternaam: { type: String, required: true },
-        woonplaats: { type: String, required: true },
-        postcode: { type: String, required: true },
-        adres: { type: String, required: true },
-        huisnummer: { type: Number, required: true },
-        telefoon: { type: String, required: true },
-        email: { type: String, required: true }
+        voornaam: {type: String, required: true},
+        achternaam: {type: String, required: true},
+        woonplaats: {type: String, required: true},
+        postcode: {type: String, required: true},
+        adres: {type: String, required: true},
+        huisnummer: {type: Number, required: true},
+        telefoon: {type: String, required: true},
+        email: {type: String, required: true}
     },
-    {
-        collection: 'algemeneGegevens'
-    });
+        {
+            collection: 'algemeneGegevens'
+        });
 
     aanvullendeGegevens = new Schema({
-        religie: { type: String, required: false },
-        Donor: { type: String, required: true }
+        religie: {type: String, required: false},
+        Donor: {type: String, required: true}
     },
-    {
-        collection: 'aanvullendeGegevens'
-    });
+        {
+            collection: 'aanvullendeGegevens'
+        });
 
     muziek = new Schema({
-        playlistId: { type: String, required: false }
+        playlistId: {type: String, required: false}
     },
-    {
-        collection: 'muziek'
-    });
+        {
+            collection: 'muziek'
+        });
 
     fotos = new Schema({
-        bestandsnaam: { type: String, required: true },
-        volgnummer: { type: Number, required: true }
+        bestandsnaam: {type: String, required: true},
+        volgnummer: {type: Number, required: true}
     },
-    {
-        collection: 'fotos'
-    });
+        {
+            collection: 'fotos'
+        });
 
     uitvaart = new Schema({
-        locatie: { type: String, required: true },
-        duurOpbaring: { type: Number, required: true },
-        beschrijvingOpbaring: { type: Number, required: true }
+        locatie: {type: String, required: true},
+        duurOpbaring: {type: Number, required: true},
+        beschrijvingOpbaring: {type: Number, required: true}
     },
-    {
-        collection:
-            'uitvaart'
-    });
+        {
+            collection: 'uitvaart'
+        });
 
     wishlist = new Schema({
-        bestandsnaam: { type: String, required: true },
-        beschrijving: { type: String, required: true },
-        content: { type: String, required: true }
+        bestandsnaam: {type: String, required: true},
+        beschrijving: {type: String, required: true},
+        content: {type: String, required: true}
     },
-    {
-        collection:
-            'wishlist'
-    });
+        {
+            collection: 'wishlist'
+        });
 
     notificatie = new Schema({
-        naam: { type: String, required: true },
-        email: { type: String, required: true },
-        bericht: { type: String, required: true }
+        naam: {type: String, required: true},
+        email: {type: String, required: true},
+        bericht: {type: String, required: true}
     },
-    {
-        collection:
-            'notificatie'
-    });
+        {
+            collection: 'notificatie'
+        });
 
     segment = new Schema({
-        object: { type: String, required: true },
-        percentage: { type: String, required: true },
-        volgnummer: { type: String, required: true }
-
+        object: {type: String, required: true},
+        percentage: {type: String, required: true},
+        volgnummer: {type: String, required: true}
     },
-    {
-        collection:
-            'segment'
-    });
+        {
+            collection: 'segment'
+        });
 
 
     uitvaartSamenstellen = new Schema({
-        tijdsduur: { type: Number, required: true },
+        tijdsduur: {type: Number, required: true},
         segment: [segment]
     },
-    {
-        collection:
-            'uitvaartSamenstellen'
-    });
+        {
+            collection: 'uitvaartSamenstellen'
+        });
 
 
     gebruiker = new Schema({
-        gebruikersnaam: { type: String, required: true, unique: true },
-        wachtwoord: { type: String, required: true },
+        gebruikersnaam: {type: String, required: true, unique: true},
+        wachtwoord: {type: String, required: true},
         algemeneGegevens: [algemeneGegevens],
         aanvullendeGegevens: [aanvullendeGegevens],
         uitvaart: [uitvaart],
@@ -121,18 +116,16 @@
         notificatie: [notificatie],
         uitvaartSamenstellen: [uitvaartSamenstellen]
     },
-    {
-        collection:
-            'gebruiker'
-    });
+        {
+            collection: 'gebruiker'
+        });
 
     dood = new Schema({
         gebruiker: [gebruiker]
     },
-    {
-        collection:
-            'dood'
-    });
+        {
+            collection: 'dood'
+        });
 
     modelName = "Dood";
     module.exports = mongoose.model(modelName, dood);
