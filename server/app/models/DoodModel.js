3650f2bf3187888193d6553c88a607b09161f1a0
ejw.nijhof@student.han.nl <ejw.nijhof@student.han.nl>
2015-05-28 15:07:25 +0200

    Alle velden met uitzondering van gebruikersnaam en wachtwoord zijn nu niet required

--- server/app/models/DoodModel.js
+++ server/app/models/DoodModel.js
@@ -20,20 +20,20 @@
         modelName;
 
     algemeneGegevens = new Schema({
-        voornaam: {type: String, required: true},
-        achternaam: {type: String, required: true},
-        woonplaats: {type: String, required: true},
-        postcode: {type: String, required: true},
-        adres: {type: String, required: true},
-        huisnummer: {type: Number, required: true},
-        telefoon: {type: String, required: true},
-        email: {type: String, required: true}
+        voornaam: {type: String, required: false},
+        achternaam: {type: String, required: false},
+        woonplaats: {type: String, required: false},
+        postcode: {type: String, required: false},
+        adres: {type: String, required: false},
+        huisnummer: {type: Number, required: false},
+        telefoon: {type: String, required: false},
+        email: {type: String, required: false}
     },
         { collection: 'algemeneGegevens' });
 
     aanvullendeGegevens = new Schema({
         religie: {type: String, required: false},
-        Donor: {type: String, required: true}
+        Donor: {type: String, required: false}
     },
         { collection: 'aanvullendeGegevens' });
 
@@ -43,42 +43,42 @@
         { collection: 'muziek' });
 
     fotos = new Schema({
-        bestandsnaam: {type: String, required: true},
-        volgnummer: {type: Number, required: true}
+        bestandsnaam: {type: String, required: false},
+        volgnummer: {type: Number, required: false}
     },
         { collection: 'fotos' });
 
     uitvaart = new Schema({
-        locatie: {type: String, required: true},
-        duurOpbaring: {type: Number, required: true},
-        beschrijvingOpbaring: {type: Number, required: true}
+        locatie: {type: String, required: false},
+        duurOpbaring: {type: Number, required: false},
+        beschrijvingOpbaring: {type: Number, required: false}
     },
         { collection: 'uitvaart' });
 
     wishlist = new Schema({
-        bestandsnaam: {type: String, required: true},
-        beschrijving: {type: String, required: true},
-        content: {type: String, required: true}
+        bestandsnaam: {type: String, required: false},
+        beschrijving: {type: String, required: false},
+        content: {type: String, required: false}
     },
         { collection: 'wishlist' });
 
     notificatie = new Schema({
-        naam: {type: String, required: true},
-        email: {type: String, required: true},
-        bericht: {type: String, required: true}
+        naam: {type: String, required: false},
+        email: {type: String, required: false},
+        bericht: {type: String, required: false}
     },
         { collection: 'notificatie' });
 
     segment = new Schema({
-        object: {type: String, required: true},
-        percentage: {type: String, required: true},
-        volgnummer: {type: String, required: true}
+        object: {type: String, required: false},
+        percentage: {type: String, required: false},
+        volgnummer: {type: String, required: false}
     },
         { collection: 'segment' });
 
 
     uitvaartSamenstellen = new Schema({
-        tijdsduur: {type: Number, required: true},
+        tijdsduur: {type: Number, required: false},
         segment: [segment]
     },
         { collection: 'uitvaartSamenstellen' });
