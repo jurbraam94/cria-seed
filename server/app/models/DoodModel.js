66e60046871933a75644fbaf1c49bd79b08d0b81
jurbraam94 <jur_braam@hotmail.com>
2015-05-29 12:35:15 +0200

    formatted model

--- server/app/models/DoodModel.js
+++ server/app/models/DoodModel.js
@@ -20,80 +20,80 @@
         modelName;
 
     algemeneGegevens = new Schema({
-            voornaam: {type: String, required: false},
-            achternaam: {type: String, required: false},
-            woonplaats: {type: String, required: false},
-            postcode: {type: String, required: false},
-            adres: {type: String, required: false},
-            huisnummer: {type: Number, required: false},
-            telefoon: {type: String, required: false},
-            email: {type: String, required: false}
-        },
+        voornaam: {type: String, required: false},
+        achternaam: {type: String, required: false},
+        woonplaats: {type: String, required: false},
+        postcode: {type: String, required: false},
+        adres: {type: String, required: false},
+        huisnummer: {type: Number, required: false},
+        telefoon: {type: String, required: false},
+        email: {type: String, required: false}
+    },
         { collection: 'algemeneGegevens' });
 
     aanvullendeGegevens = new Schema({
-            religie: {type: String, required: false},
-            Donor: {type: String, required: false}
-        },
+        religie: {type: String, required: false},
+        Donor: {type: String, required: false}
+    },
         { collection: 'aanvullendeGegevens' });
 
     muziek = new Schema({
-            playlistId: {type: String, required: false}
-        },
+        playlistId: {type: String, required: false}
+    },
         { collection: 'muziek' });
 
     fotos = new Schema({
-            bestandsnaam: {type: String, required: false},
-            volgnummer: {type: Number, required: false}
-        },
+        bestandsnaam: {type: String, required: false},
+        volgnummer: {type: Number, required: false}
+    },
         { collection: 'fotos' });
 
     uitvaart = new Schema({
-            locatie: {type: String, required: false},
-            duurOpbaring: {type: Number, required: false},
-            beschrijvingOpbaring: {type: Number, required: false}
-        },
+        locatie: {type: String, required: false},
+        duurOpbaring: {type: Number, required: false},
+        beschrijvingOpbaring: {type: Number, required: false}
+    },
         { collection: 'uitvaart' });
 
     wishlist = new Schema({
-            bestandsnaam: {type: String, required: false},
-            beschrijving: {type: String, required: false},
-            content: {type: String, required: false}
-        },
+        bestandsnaam: {type: String, required: false},
+        beschrijving: {type: String, required: false},
+        content: {type: String, required: false}
+    },
         { collection: 'wishlist' });
 
     notificatie = new Schema({
-            naam: {type: String, required: false},
-            email: {type: String, required: false},
-            bericht: {type: String, required: false}
-        },
+        naam: {type: String, required: false},
+        email: {type: String, required: false},
+        bericht: {type: String, required: false}
+    },
         { collection: 'notificatie' });
 
     segment = new Schema({
-            object: {type: String, required: false},
-            percentage: {type: String, required: false},
-            volgnummer: {type: String, required: false}
-        },
+        object: {type: String, required: false},
+        percentage: {type: String, required: false},
+        volgnummer: {type: String, required: false}
+    },
         { collection: 'segment' });
 
     uitvaartSamenstellen = new Schema({
-            tijdsduur: {type: Number, required: false},
-            segment: [{type: mongoose.Schema.Types.ObjectId, ref: 'segment'}]
-        },
+        tijdsduur: {type: Number, required: false},
+        segment: [{type: mongoose.Schema.Types.ObjectId, ref: 'segment'}]
+    },
         { collection: 'uitvaartSamenstellen' });
 
     gebruiker = new Schema({
-            gebruikersnaam: {type: String, required: true, unique: true},
-            wachtwoord: {type: String, required: true},
-            algemeneGegevens: {type: mongoose.Schema.Types.ObjectId, ref: 'algemeneGegevens'},
-            aanvullendeGegevens: {type: mongoose.Schema.Types.ObjectId, ref: 'aanvullendeGegevens'},
-            uitvaart: {type: mongoose.Schema.Types.ObjectId, ref: 'uitvaart'},
-            muziek: [{type: mongoose.Schema.Types.ObjectId, ref: 'muziek'}],
-            fotos: [{type: mongoose.Schema.Types.ObjectId, ref: 'fotos'}],
-            wishlist: [{type: mongoose.Schema.Types.ObjectId, ref: 'wishlist'}],
-            notificatie: [{type: mongoose.Schema.Types.ObjectId, ref: 'notificatie'}],
-            uitvaartSamenstellen: {type: mongoose.Schema.Types.ObjectId, ref: 'uitvaartSamenstellen'}
-        },
+        gebruikersnaam: {type: String, required: true, unique: true},
+        wachtwoord: {type: String, required: true},
+        algemeneGegevens: {type: mongoose.Schema.Types.ObjectId, ref: 'algemeneGegevens'},
+        aanvullendeGegevens: {type: mongoose.Schema.Types.ObjectId, ref: 'aanvullendeGegevens'},
+        uitvaart: {type: mongoose.Schema.Types.ObjectId, ref: 'uitvaart'},
+        muziek: [{type: mongoose.Schema.Types.ObjectId, ref: 'muziek'}],
+        fotos: [{type: mongoose.Schema.Types.ObjectId, ref: 'fotos'}],
+        wishlist: [{type: mongoose.Schema.Types.ObjectId, ref: 'wishlist'}],
+        notificatie: [{type: mongoose.Schema.Types.ObjectId, ref: 'notificatie'}],
+        uitvaartSamenstellen: {type: mongoose.Schema.Types.ObjectId, ref: 'uitvaartSamenstellen'}
+    },
         { collection: 'gebruiker' });
 
     console.log(algemeneGegevens, aanvullendeGegevens, uitvaart, muziek, fotos, wishlist, notificatie, uitvaartSamenstellen, segment);
