b6db34bf023c01db7f1256e0bea8a292bdb68e74
jurbraam94 <jur_braam@hotmail.com>
2015-05-31 22:03:17 +0200

    made login client side

--- client/javascripts/services.js
+++ client/javascripts/services.js
@@ -10,12 +10,13 @@
                     'get': {method: 'GET'},
                     'save': {method: 'POST'},
                     'query': {method: 'GET', isArray: true},
+                    'login': { method: 'GET', url: '/api/gebruiker/_gebruikersnaam/_wachtwoord'},
                     'update': {method: 'PUT'},
                     'delete': {method: 'DELETE'}
                 },
                 db = {};
             // REST url to server
-            db.gebruiker = $resource('/api/gebruiker/:_gebruikersnaam/:_wachtwoord', {}, actions);
+            db.gebruiker = $resource('/api/gebruiker/', {}, actions);
             return db;
         }]);
 }());
