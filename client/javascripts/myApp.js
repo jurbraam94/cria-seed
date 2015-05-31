2a7b65fd8178570bdf5687b794814a8c24b8b4ce
Safi <s.rasuli@student.han.nl>
2015-05-31 20:49:57 +0200

    Angular werkt weer

---
+++ client/javascripts/myApp.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Gebruiker on 31-5-2015.
+ */
+var myAppModule = angular.module('myApp1', []);
+
+
+function haalAfbeelding() {
+
+}
+
+myAppModule.controller("SamenstellenController", function ($scope){
+    var images;
+
+
+    $scope.plaatje = 'style/images/icons/muziek.png';
+
+
+
+
+});
+
+
+myAppModule.run(function()){
+
+
+}
