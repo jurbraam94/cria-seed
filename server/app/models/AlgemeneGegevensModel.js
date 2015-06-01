6b35d65405144b918ab200394481cd5b661df7ee
jurbraam94 <jur_braam@hotmail.com>
2015-06-01 10:42:22 +0200

    made login client side

--- server/app/models/AlgemeneGegevensModel.js
+++ server/app/models/AlgemeneGegevensModel.js
@@ -11,14 +11,13 @@
     var mongoose = require('mongoose'),
         Schema = mongoose.Schema,
         algemeneGegevens,
-        modelName = "AlgemeneGegevens",
-        gebruiker = require('');
+        modelName = "AlgemeneGegevens";
 
     algemeneGegevens = new Schema({
         gebruikersnaam: {type: String, required: true, unique: true},
         voornaam: {type: String, required: true, minlength: 2, maxlength: 255},
-        achternaam: {type: String, required: true, minlength:2, maxlength: 255},
-        woonplaats: {type: String, required: true, minlength:2, maxlength: 255},
+        achternaam: {type: String, required: true, minlength: 2, maxlength: 255},
+        woonplaats: {type: String, required: true, minlength: 2, maxlength: 255},
         postcode: {type: String, required: true, minlength: 6, maxlength: 6, validate: /^[1-9]\d{3}[A-Z]{2}$/},
         adres: {type: String, required: true},
         huisnummer: {type: Number, required: true},
