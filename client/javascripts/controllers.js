35872470c5a779e18b62b028a51b794ee69bcf50
jurbraam94 <jur_braam@hotmail.com>
2015-06-01 21:25:30 +0200

    made autorisation

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -11,18 +11,18 @@
 myApp.controller('GebruikerLoginController', function ($scope, $routeParams, $location, gebruikersnaamService, $cookieStore) {
     "use strict";
 
-    if ($cookieStore.get('sessionCookie') !== undefined) {
+    /*if ($cookieStore.get('sessionCookie') !== undefined) {
         $scope.loggedIn = true;
     } else {
         $scope.loggedIn = false;
-    }
+    }*/
 
     // LOGIN
     $scope.login = function (gebruiker) {
         $scope.gebruiker = gebruikersnaamService.gebruiker.login({gebruikersnaam: gebruiker.gebruikersnaam, wachtwoord: gebruiker.wachtwoord}, function () {
-            console.log($scope.gebruiker);
+            console.log($scope.gebruiker.doc.gebruikersnaam);
             if ($scope.gebruiker !== null) {
-                $cookieStore.put('sessionCookie', $scope.gebruiker.gebruikersnaam);
+                $cookieStore.put('sessionCookie', $scope.gebruiker.doc.gebruikersnaam);
             }
         });
     };
