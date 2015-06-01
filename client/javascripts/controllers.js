850ec3866c187892d8cb54a3c3d3dafffa1980f2
jurbraam94 <jur_braam@hotmail.com>
2015-06-01 19:39:25 +0200

    new angular files

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -14,7 +14,7 @@ myApp.controller('GebruikerLoginController', function ($scope, $routeParams, $lo
     $scope.login = function (gebruiker) {
         $scope.gebruiker = gebruikersnaamService.gebruiker.login({gebruikersnaam: gebruiker.gebruikersnaam, wachtwoord: gebruiker.wachtwoord}, function () {
             console.log($scope.gebruiker);
-            if ($scope.gebruiker !== null) {
+            if ($scope.gebruiker.err === null) {
                 $cookieStore.put('sessionCookie', $scope.gebruiker);
             }
         });
