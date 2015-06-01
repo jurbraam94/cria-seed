2698e2a25e4a61b2a5620e160f8975d7f2e23247
ejw.nijhof@student.han.nl <ejw.nijhof@student.han.nl>
2015-06-01 11:18:07 +0200

    email regex aangepast

--- server/app/models/NotificatieModel.js
+++ server/app/models/NotificatieModel.js
@@ -8,7 +8,7 @@
     var mongoose = require('mongoose'),
         Schema = mongoose.Schema,
         notificatie,
-        emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         modelName = "Notificatie";
 
     /**
