f0f99a3c1d99e73d96940f15bfa4c11563f560dc
jurbraam94 <jur_braam@hotmail.com>
2015-06-01 11:53:21 +0200

    ieks

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -12,7 +12,7 @@ myApp.controller('gebruikerLoginCtrl', function ($scope, $routeParams, $location
     "use strict";
     // LOGIN
     $scope.login = function () {
-        $scope.gebruiker = gebruikersnaamService.gebruiker.login({gebruikersnaam: $routeParams._gebruikersnaam, wachtwoord: $routeParams._wachtwoord}, function () {
+        $scope.gebruiker = gebruikersnaamService.gebruiker.login({gebruikersnaam: $routeParams.gebruikersnaam, wachtwoord: $routeParams.wachtwoord}, function () {
             console.log($scope.gebruiker);
         });
     };
