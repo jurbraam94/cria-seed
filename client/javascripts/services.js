7345077bf62e742f31f126fb98683ed9f881cfd1
Safi <s.rasuli@student.han.nl>
2015-06-03 12:24:07 +0200

    Samenstellen pagina doet het weer.

--- client/javascripts/services.js
+++ client/javascripts/services.js
@@ -1,27 +1,6 @@
 /*jslint node: true */
 /*global angular */
 
-(function () {
-    "use strict";
-
-    angular.module('myApp.services', ['ngResource']).factory('gebruikersnaamService', ['$resource', '$http',
-
-        function ($resource) {
-            var actions = {
-                    'get': {method: 'GET'},
-                    'save': {method: 'POST'},
-                    'query': {method: 'GET', isArray: true},
-                    'login': { method: 'GET', params: {gebruikersnaam: 'gebruikersnaam', wachtwoord: 'wachtwoord'}},
-                    'update': {method: 'PUT'},
-                    'delete': {method: 'DELETE'}
-                },
-                db = {};
-            // REST url to server
-            db.gebruiker = $resource('/api/gebruiker/:gebruikersnaam/:wachtwoord', {}, actions);
-            db.uitvaartSamenstellen = $resource('/api/uitvaartSamenstellen', {}, actions);
-            return db;
-        }]);
-}());
 
 angular.module('myApp.services', ['ngResource']).factory('Api', ['$resource', '$http',
         function ($resource) {
@@ -40,6 +19,14 @@ angular.module('myApp.services', ['ngResource']).factory('Api', ['$resource', '$
                         method: 'POST',
                         params: {gebruikersnaam: 'gebruikersnaam', tijdsduur: 'tijdsduur'}
                     }
-                })
+                })/*,
+            uitvaartSegmentToevoegen: $resource('/api/segment',
+                {
+                    'Create': {
+                        method: 'POST',
+                        params
+                    }
+
+                })*/
         };
     }]);
