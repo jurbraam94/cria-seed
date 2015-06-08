b95e80afcc74673718bda6534e067b23936b4cc6
ejw.nijhof@student.han.nl <ejw.nijhof@student.han.nl>
2015-06-08 18:58:17 +0200

    testen of het allemaal werkt

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -341,7 +341,6 @@ myApp.controller('SamenstellenController', function ($scope, DOODService, $route
             totaleTijd = 90;
         }
         getDataTableUitDb();
-        console.log("dataTable: ", dataTable);
         drawChart();
     };
 
