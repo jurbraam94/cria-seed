9d40eb639fd0ce32d89f226444e3baae7fd99129
ejw.nijhof@student.han.nl <ejw.nijhof@student.han.nl>
2015-06-15 15:40:57 +0200

    we hebben al een voldoende

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -239,15 +239,15 @@ myApp.controller('SamenstellenController', function ($scope, DOODService, $route
         },
 
         chartActies = function (id, nieuweWaarde) {
-            console.log("getSelection[0] = ", chart.getSelection()[0]);
-            if (chart.getSelection()[0] === "Overige tijd") {
-                return;
-            }
             chart.setAction({
                 id: id,
                 text: id,
                 action: function () {
                     var index = chart.getSelection()[0].row + 1;
+                    console.log("getSelection[0] = ", chart.getSelection()[0]);
+                    if (chart.getSelection()[0] === "Overige tijd") {
+                        return;
+                    }
                     if (nieuweWaarde === 0) {
                         dataTable[index][1] = 0;
                         dataTable.splice(index, 1);
