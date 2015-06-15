02f76d0aa2d6e7933bd77d84cc60851ac0a7f4a0
ejw.nijhof@student.han.nl <ejw.nijhof@student.han.nl>
2015-06-15 11:51:43 +0200

    we hebben al een voldoende

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -222,7 +222,11 @@ myApp.controller('SamenstellenController', function ($scope, DOODService, $route
             var i;
             for (i = 0; i < (dataTable.length - 1); i += 1) {
                 if (kleuren[i] === null || kleuren[i] === undefined) {
-                    kleuren[i] = '#' + Math.random().toString(16).slice(2, 8);
+                    if (dataTable[i][0] !== 'Overige tijd') {
+                        kleuren[i] = '#' + Math.random().toString(16).slice(2, 8);
+                    } else {
+                        kleuren[i] = '#afafaf';
+                    }
                 }
             }
         },
@@ -244,6 +248,7 @@ myApp.controller('SamenstellenController', function ($scope, DOODService, $route
                 }
             });
         },
+
         drawChart = function (changed) {
             var data, options;
 
