a474ce8adb7739c87cca9f51b73286858756b632
ejw.nijhof@student.han.nl <ejw.nijhof@student.han.nl>
2015-06-15 14:14:54 +0200

    we hebben al een voldoende

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -231,7 +231,7 @@ myApp.controller('SamenstellenController', function ($scope, DOODService, $route
                 if (kleuren[j] === null) {
                     kleuren[j] = '#' + Math.random().toString(16).slice(2, 8);
                     console.log("kleur van ", dataTable[i][0], " is nu ", kleuren[j]);
-                } else if (dataTable[i][0] !== 'Overige tijd') {
+                } else if (dataTable[i][0] === 'Overige tijd') {
                     kleuren[j] = '#afafaf';
                     console.log("Overige tijd(?) kleur van ", dataTable[i][0], " is nu ", kleuren[j]);
                 }
