6cdabda00f01e753c5e74a54f0518eac1cfc3839
jurbraam94 <jur_braam@hotmail.com>
2015-06-16 13:40:15 +0200

    changed formulier functien

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -74,9 +74,34 @@ myApp.controller('formulierController', function ($scope, DOODService) {
                     if (uitvaartGet.doc !== null) {
                         $scope.formulierData.uitvaart = uitvaartGet.doc;
                         locatie = uitvaartGet.doc.locatie.split(",");
-                        latitude = locatie[0];
-                        longitude = locatie[1];
-                        console.log(latitude, longitude);
+
+                        //google maps stuff
+                        $scope.map = {
+                            "center": {
+                                latitude: locatie[0],
+                                longitude:  locatie[1]
+                            },
+                            "zoom": 15
+                        };
+                        $scope.marker = {
+                            id: 0,
+                            coords: {
+                                latitude: locatie[0],
+                                longitude: locatie[1]
+                            },
+                            options: { draggable: true },
+                            events: {
+                                dragend: function (marker, eventName, args) {
+
+                                    $scope.marker.options = {
+                                        draggable: true,
+                                        labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                                        labelAnchor: "100 0",
+                                        labelClass: "marker-labels"
+                                    };
+                                }
+                            }
+                        };
                     } else {
                         DOODService.uitvaartPost.post({gebruikersnaam: gebruiker.doc.gebruikersnaam});
                     }
@@ -117,34 +142,6 @@ myApp.controller('formulierController', function ($scope, DOODService) {
         });
     };
 
-    console.log(latitude || "hoi", longitude || "hoi");
-    //google maps stuff
-    $scope.map = {
-        "center": {
-            latitude: latitude || 52.00494,
-            longitude: longitude || 5.91968
-        },
-        "zoom": 15
-    };
-    $scope.marker = {
-        id: 0,
-        coords: {
-            latitude: latitude || 52.00494,
-            longitude: longitude || 5.91968
-        },
-        options: { draggable: true },
-        events: {
-            dragend: function (marker, eventName, args) {
-
-                $scope.marker.options = {
-                    draggable: true,
-                    labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-                    labelAnchor: "100 0",
-                    labelClass: "marker-labels"
-                };
-            }
-        }
-    };
     events = {
         places_changed: function (searchBox) {
 
