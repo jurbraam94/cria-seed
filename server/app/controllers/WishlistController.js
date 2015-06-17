67b676ae4c66a941bda683d58cac84272c0d1fb6
jurbraam94 <jur_braam@hotmail.com>
2015-06-17 20:15:13 +0200

    added unit tests

--- server/app/controllers/WishlistController.js
+++ server/app/controllers/WishlistController.js
@@ -64,7 +64,7 @@ exports.alleWishlists = function (req, res) {
 exports.wishlistVerwijderen = function (req, res) {
     var conditions, callback, retObj;
 
-    conditions = {gebruikersnaam: req.params._gebruikersnaam, bericht: req.params._titel};
+    conditions = {gebruikersnaam: req.params._gebruikersnaam, titel: req.params._titel};
 
     callback = function (err, doc) {
         retObj = {
